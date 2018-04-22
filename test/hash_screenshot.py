55797ea4ec814d384828c66a9db107ef72e1e58a
Chris Drappier <chris.drappier@gmail.com>
2018-04-22 11:27:53 -0700

    the beginnings of automated testing for the site.

---
+++ test/hash_screenshot.py
@@ -0,0 +1,17 @@
+import hashlib
+
+def hash_file(filename):
+    hasher = hashlib.md5()
+    with open(filename, 'rb') as afile:
+        buf = afile.read()
+        hasher.update(buf)
+    return hasher.hexdigest()
+
+screenshot = hash_file('screenshot.png')
+screenshot_baseline = hash_file('baseline-screenshot.png')
+
+equal = screenshot == screenshot_baseline
+
+print("screenshot          : {}".format(screenshot))
+print("screenshot_baseline : {}".format(screenshot_baseline))
+print("equal?              : {}".format(equal))
